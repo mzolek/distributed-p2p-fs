583c4b073410c3c029f6a709f8f8711178d9403b
Krzysztof Żyndul <krzyszyndul@gmail.com>
2025-06-16 11:43:37 +0200

    sharing files

--- tree.go
+++ tree.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"fmt"
 	"image"
 	"image/jpeg"
@@ -289,3 +290,252 @@ func isBigDirectory(node *Node) int {
 
 	return result
 }
+
+// convert file from given path to chunks
+// returns hash of the file or big
+func fileToHash(path string, chunkToHash map[[32]byte][]byte) ([32]byte, error) {
+	fileBytes, err := os.ReadFile(path)
+	if err != nil {
+		return [32]byte{}, fmt.Errorf("error reading file %s: %v", path, err)
+	}
+
+	fmt.Printf("Processing file: %s\n", path)
+	fmt.Printf("File content (as bytes): %x\n", fileBytes)
+	fmt.Printf("File content (as string): %s", fileBytes)
+
+	chunkList := make([]byte, 0)
+	chunkListTmp := make([]byte, 0)
+
+	// empty file case
+	if len(fileBytes) == 0 {
+		chunk := []byte{byte(Chunk)}
+		chunkHash := sha256.Sum256(chunk)
+		chunkToHash[chunkHash] = chunk
+		fmt.Printf("Empty file hash: %x\n\n", chunkHash)
+		return chunkHash, nil
+	}
+
+	// Splitting file into chunks of maximal size of 1024 bytes
+	for i := 0; i < len(fileBytes); i += 1024 {
+		end := min(i+1024, len(fileBytes))
+		fileSplit := fileBytes[i:end]
+		chunk := append([]byte{byte(Chunk)}, fileSplit...)
+		chunkHash := sha256.Sum256(chunk)
+		chunkList = append(chunkList, chunkHash[:]...)
+		chunkToHash[chunkHash] = chunk
+	}
+
+	// If the file is longer then 1024 bytes, we need to use Big type
+	// We iterate until we have only one hash left in chunkList that is the
+	// final hash of the file
+	for len(chunkList) > 32 {
+
+		// We need to fill big with hashes
+		for len(chunkList) > 0 {
+			end := min(32*32, len(chunkList))
+
+			bigSplit := chunkList[:end]
+			chunk := append([]byte{byte(Big)}, bigSplit...)
+
+			chunkHash := sha256.Sum256(chunk)
+			chunkListTmp = append(chunkListTmp, chunkHash[:]...)
+			chunkToHash[chunkHash] = chunk
+			chunkList = chunkList[end:]
+		}
+
+		chunkList = append(chunkList, chunkListTmp...)
+		chunkListTmp = make([]byte, 0)
+	}
+	fmt.Printf("File hash: %x\n\n", chunkList[:32])
+
+	return [32]byte(chunkList[:32]), nil
+}
+
+// Add directory to Merkle tree
+func DirToHash(path string, chunkToHash map[[32]byte][]byte) ([32]byte, error) {
+	dirBytes, err := os.ReadDir(path)
+	if err != nil {
+		return [32]byte{}, fmt.Errorf("error reading directory %s: %v", path, err)
+	}
+
+	chunkList := make([]byte, 0)
+
+	// Process each entry in the directory and recursively convert it to hash
+	for _, entry := range dirBytes {
+		if entry.IsDir() {
+			subDirPath := path + "/" + entry.Name()
+			subDirHash, err := DirToHash(subDirPath, chunkToHash)
+			if err != nil {
+				return [32]byte{}, fmt.Errorf("error processing subdirectory %s: %v", subDirPath, err)
+			}
+			chunkList = append(chunkList, nameToBytes(entry.Name())...)
+			chunkList = append(chunkList, subDirHash[:]...)
+		} else {
+			filePath := path + "/" + entry.Name()
+			fileHash, err := fileToHash(filePath, chunkToHash)
+			if err != nil {
+				return [32]byte{}, fmt.Errorf("error processing file %s: %v", filePath, err)
+			}
+			chunkList = append(chunkList, nameToBytes(entry.Name())...)
+			chunkList = append(chunkList, fileHash[:]...)
+		}
+
+	}
+
+	// empty dir case
+	if len(chunkList) == 0 {
+		chunk := []byte{byte(Directory)}
+		chunkHash := sha256.Sum256(chunk)
+		chunkToHash[chunkHash] = chunk
+		fmt.Printf("Empty directory hash: %x\n\n", chunkHash)
+		return chunkHash, nil
+	}
+
+	// Splitting directory into groups of maximal size 16 and calculating hash
+	chunkListTmp := make([]byte, 0)
+	for len(chunkList) > 0 {
+		end := min(16*64, len(chunkList))
+
+		dirSplit := chunkList[:end]
+		chunk := append([]byte{byte(Directory)}, dirSplit...)
+		chunkHash := sha256.Sum256(chunk)
+
+		chunkListTmp = append(chunkListTmp, chunkHash[:]...)
+		chunkToHash[chunkHash] = chunk
+		chunkList = chunkList[end:]
+	}
+	chunkList = append(chunkList, chunkListTmp...)
+	chunkListTmp = make([]byte, 0)
+
+	// If the directory has more then 16 entries, we need to use Big type
+	// We iterate until we have only one hash left in chunkList that is the
+	// final hash of the directory
+	for len(chunkList) > 32 {
+
+		// We need to fill big with hashes
+		for len(chunkList) > 0 {
+			end := min(32*32, len(chunkList))
+			bigSplit := chunkList[:end]
+			chunk := append([]byte{byte(Big)}, bigSplit...)
+
+			chunkHash := sha256.Sum256(chunk)
+			chunkListTmp = append(chunkListTmp, chunkHash[:]...)
+			chunkToHash[chunkHash] = chunk
+			chunkList = chunkList[end:]
+		}
+
+		chunkList = append(chunkList, chunkListTmp...)
+		chunkListTmp = make([]byte, 0)
+	}
+	fmt.Printf("Dir hash: %x\n\n", chunkList[:32])
+
+	return [32]byte(chunkList[:32]), nil
+}
+
+// Creates a root hash for the file system rooted at a given path and collects
+// hashes for Merkle tree construction.
+func createFileSystemHash(path string, chunkToHash map[[32]byte][]byte) ([32]byte, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return [32]byte{}, fmt.Errorf("path %s does not exist", path)
+	}
+
+	info, err := os.Stat(path)
+	if err != nil {
+		return [32]byte{}, fmt.Errorf("error getting info for path %s: %v", path, err)
+	}
+	if info.IsDir() {
+		fmt.Printf("[createFileSystemHash] Processing directory: %s\n", path)
+		return DirToHash(path, chunkToHash)
+	} else {
+		fmt.Printf("[createFileSystemHash] Processing file: %s\n", path)
+		return fileToHash(path, chunkToHash)
+	}
+}
+
+func nameToBytes(name string) []byte {
+	if len(name) > 32 {
+		name = name[:32]
+	}
+	nameBytes := []byte(name)
+	if len(nameBytes) < 32 {
+		padding := make([]byte, 32-len(nameBytes))
+		nameBytes = append(nameBytes, padding...)
+	}
+	return nameBytes
+}
+
+func printDebugInfo(hash [32]byte, chunkToHash map[[32]byte][]byte, indent string) {
+	chunk, exists := chunkToHash[hash]
+	if !exists {
+		fmt.Printf("Hash %x not found in chunkToHash map\n", hash)
+		return
+	}
+
+	if chunk[0] == Chunk {
+		fmt.Printf("%sChunk found: %x\n", indent, hash)
+	} else if chunk[0] == Directory {
+
+		fmt.Printf("%sDirectory found: %x, length: %d [", indent, hash, len(chunk))
+		for i := 1; i < len(chunk); i += 64 {
+			fmt.Printf(" %s-", string(chunk[i:i+32]))
+			fmt.Printf("%x", chunk[i+32:i+64])
+		}
+		fmt.Printf(" ]\n")
+
+		for i := 1; i < len(chunk); i += 64 {
+
+			data := chunkToHash[[32]byte(chunk[i+32:i+64])]
+
+			if data[0] == Chunk {
+				fmt.Printf("%sChunk entry name: %s data: %x, hash: %x\n", indent+"----", chunk[i:i+32], data, chunk[i+32:i+64])
+			} else if data[0] == Directory {
+				printDebugInfo([32]byte(chunk[i+32:i+64]), chunkToHash, indent+"----")
+				// fmt.Printf("%sName %s\n", indent, string(chunk[i:i+32]))
+				// fmt.Printf("%sBytes %x\n", indent, chunk[i+32:i+64])
+			} else if data[0] == Big {
+				printDebugInfo([32]byte(chunk[i+32:i+64]), chunkToHash, indent)
+			} else {
+				fmt.Printf("%sUnknown entry type: %x, index: %d\n", indent, chunk[i], i)
+			}
+		}
+	} else if chunk[0] == Big {
+		fmt.Printf("%sBig entry data: [", indent+"----")
+
+		for i := 1; i < len(chunk); i += 32 {
+			fmt.Printf(" %x", chunk[i:i+32])
+		}
+		fmt.Printf(" ]\n")
+
+		for i := 1; i < len(chunk); i += 32 {
+			data := chunkToHash[[32]byte(chunk[i:i+32])]
+			if data[0] == Chunk {
+				fmt.Printf("%sChunk entry data: %x, hash: %x\n", indent+"----", data, chunk[i:i+32])
+			} else if data[0] == Directory {
+				printDebugInfo([32]byte(chunk[i:i+32]), chunkToHash, indent+"----")
+			} else if data[0] == Big {
+				printDebugInfo([32]byte(chunk[i:i+32]), chunkToHash, indent+"----")
+			} else {
+				fmt.Printf("%sUnknown entry type: %x, index: %d\n", indent+"----", chunk[i], i)
+			}
+		}
+	}
+}
+
+// func main() {
+// 	if len(os.Args) < 2 {
+// 		fmt.Println("Usage: go run main.go <path>")
+// 		os.Exit(1)
+// 	}
+
+// 	path := os.Args[1]
+// 	chunkToHash := make(map[[32]byte][]byte)
+
+// 	hash, err := createFileSystemHash(path, chunkToHash)
+// 	if err != nil {
+// 		fmt.Printf("Error creating file system hash: %v\n", err)
+// 		os.Exit(1)
+// 	}
+// 	fmt.Printf("File system hash for %s: %x\n", path, hash)
+
+// 	printDebugInfo(hash, chunkToHash, "")
+// }
